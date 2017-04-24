4c24aee96647011812aee830cf42fb98059a5e94
Maxime Chambreuil <mchambreuil@ursainfosystems.com>
2017-04-24 17:07:30 -0500

    [FIX] License as dependency is proprietary

--- l10n_us_check_writing_address/__manifest__.py
+++ l10n_us_check_writing_address/__manifest__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Ursa Information Systems <http://www.ursainfosystems.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     'name': 'US Check Printing with Payee Address',
     'summary': 'Print US Checks',
     'version': '10.0.1.0.0',
-    'license': 'AGPL-3',
+    'license': 'LGPL-3',
     'author': 'Ursa Information Systems, Odoo Community Association (OCA)',
     'category': 'Localization/Checks Printing',
     'maintainer': 'Ursa Information Systems',
--- l10n_us_check_writing_address/models/__init__.py
+++ l10n_us_check_writing_address/models/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Ursa Information Systems <http://www.ursainfosystems.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from . import res_partner
