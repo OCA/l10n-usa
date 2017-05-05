f63c255d6e9bf30e9f623e98087826ba95ba62a2
Maxime Chambreuil <mchambreuil@ursainfosystems.com>
2017-05-05 18:17:47 -0500

    [FIX] Module name and version

---
+++ l10n_us_account_profile/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Ursa Information Systems <http://www.ursainfosystems.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "US Accounting",
+    "summary": "Additional features to manage US accounting in Odoo",
+    "version": "10.0.0.0.1",
+    "category": "Accounting",
+    "website": "http://www.ursainfosystems.com",
+    "author": "Ursa Information Systems, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": True,
+    "installable": True,
+    "depends": [
+        "l10n_us",
+        "account_due_list",
+    ],
+}
