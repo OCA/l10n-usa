760007a6b4f79d840a75518005cb6333f9dcf4e2
Maxime Chambreuil <mchambreuil@ursainfosystems.com>
2017-05-05 18:17:47 -0500

    [FIX] Module name and version

--- l10n_us_account_profile/__manifest__.py
+++ l10n_us_account_profile/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "US Accounting",
     "summary": "Additional features to manage US accounting in Odoo",
-    "version": "10.0.1.0.0",
+    "version": "10.0.0.0.1",
     "category": "Accounting",
     "website": "http://www.ursainfosystems.com",
     "author": "Ursa Information Systems, Odoo Community Association (OCA)",
