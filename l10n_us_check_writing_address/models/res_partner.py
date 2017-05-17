9736e2e10e9404bdc64634736b22845b04781101
Bhavesh Odedra <bodedra@ursainfosystems.com>
2017-05-17 22:30:34 +0530

    [FIX] formating

--- l10n_us_check_writing_address/models/res_partner.py
+++ l10n_us_check_writing_address/models/res_partner.py
@@ -2,7 +2,9 @@
 # Copyright 2017 Ursa Information Systems <http://www.ursainfosystems.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
+
 from odoo import api, models
+
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
