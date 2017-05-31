7a5cd8dc3b8d2813cbe3606654b40eae2bf3be6e
Sandip Mangukiya <smangukiya@ursainfosystems.com>
2017-05-31 17:21:52 +0530

    [IMP]Replace dependency of l10n_us_check_printing with account_check_printing_report_base, related changes, etc

--- l10n_us_check_writing_address/report/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Ursa Information Systems <http://www.ursainfosystems.com>
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-
-from . import print_check
