f79839779066cf569965ca6660c968067b1f2353
Sandip Mangukiya <smangukiya@ursainfosystems.com>
2017-05-31 17:21:52 +0530

    [IMP]Replace dependency of l10n_us_check_printing with account_check_printing_report_base, related changes, etc

--- l10n_us_check_writing_address/__manifest__.py
+++ l10n_us_check_writing_address/__manifest__.py
@@ -1,20 +1,19 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Ursa Information Systems <http://www.ursainfosystems.com>
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'US Check Printing with Payee Address',
     'summary': 'Print US Checks',
     'version': '10.0.1.0.0',
-    'license': 'LGPL-3',
+    'license': 'AGPL-3',
     'author': 'Ursa Information Systems, Odoo Community Association (OCA)',
     'category': 'Localization/Checks Printing',
     'maintainer': 'Ursa Information Systems',
     'website': 'http://www.ursainfosystems.com',
     'depends': [
-        'l10n_us_check_printing',
+        'account_check_printing_report_base',
     ],
     'data': [
-        'report/print_check.xml',
     ],
     'installable': True,
 }
