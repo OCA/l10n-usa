67022dacad6461f9d484e18fdd2f640a3ea94a1d
Swapnesh <s.shah.serpentcs@gmail.com>
2019-06-11 11:57:22 +0530

    [FIX]l10n_us_check_writing_address: Add missing parser, Fix travis

--- l10n_us_check_writing_address/__manifest__.py
+++ l10n_us_check_writing_address/__manifest__.py
@@ -4,21 +4,21 @@
 {
     'name': 'US Check Printing with Payee Address',
     'summary': 'Print US Checks',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.1.0',
     'license': 'AGPL-3',
-    'author': 'Ursa Information Systems, Odoo Community Association (OCA)',
+    'author': 'Open Source Integrators, Odoo Community Association (OCA)',
     'category': 'Localization/Checks Printing',
-    'maintainer': 'Ursa Information Systems',
-    'website': 'http://www.ursainfosystems.com',
+    'maintainer': 'Open Source Integrators',
+    'website': 'https://www.opensourceintegrators.com',
     'depends': [
         'account_check_printing_report_base',
     ],
     'data': [
-        'report/account_check_writing_report.xml',
+        'data/account_payment_check_report_data.xml',
         'report/report_check_base_top.xml',
         'report/report_check_base_middle.xml',
         'report/report_check_base_bottom.xml',
-        'data/account_payment_check_report_data.xml',
+        'report/account_check_writing_report.xml',
     ],
     'installable': True,
 }
