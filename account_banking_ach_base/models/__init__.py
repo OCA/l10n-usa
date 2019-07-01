34ba36640e87ad28067d47d8e0379632674e1130
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-07-01 10:37:18 -0500

    [ADD] account_banking_ach_base

---
+++ account_banking_ach_base/models/__init__.py
@@ -0,0 +1,7 @@
+from . import account_banking_mandate
+from . import account_invoice
+from . import account_payment_order
+from . import base
+from . import res_bank
+from . import res_company
+from . import res_partner
