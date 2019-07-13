6e28572d0234b59e27f3e2a210bbc021ed2c27ed
brian10048 <brian@mcmpest.com>
2019-07-12 21:25:38 -0400

    [IMP] l10us_form_1099

--- l10n_us_form_1099/models/res_partner.py
+++ l10n_us_form_1099/models/res_partner.py
@@ -4,10 +4,34 @@
 from odoo import api, fields, models
 
 
+TYPES_1099 = [
+    ('a', "1099-A"),
+    ('b', "1099-B"),
+    ('c', "1099-C"),
+    ('cap', "1099-CAP"),
+    ('div', "1099-DIV"),
+    ('g', "1099-G"),
+    ('h', "1099-H"),
+    ('int', "1099-INT"),
+    ('k', "1099-K"),
+    ('ltc', "1099-LTC"),
+    ('misc', "1099-MISC"),
+    ('oid', "1099-OID"),
+    ('pair', "1099-PATR"),
+    ('q', "1099-Q"),
+    ('r', "1099-R"),
+    ('s', "1099-S"),
+    ('sa', "1099-SA"),
+    ('rrb', "RRB-1099"),
+    ('ssa', "SSA-1099"),
+]
+
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
     is_1099 = fields.Boolean('Is a 1099?')
+    type_1099 = fields.Selection(
+        TYPES_1099, string='1099 Type')
 
     @api.onchange('is_1099')
     def _on_change_is_1099(self):
