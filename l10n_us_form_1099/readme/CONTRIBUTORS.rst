950dc4decee7a5fff1cf1ed7eea20c150fb47d31
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-07-19 13:59:58 -0500

    [IMP] l10n_us_form_1099: Add 1099-MISC boxes

---
+++ l10n_us_form_1099/readme/CONTRIBUTORS.rst
@@ -0,0 +1,5 @@
+* Jenny Wu <jwu@ursainfosystems.com>
+* Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
+* Serpent Consulting Services Pvt. Ltd. <support@serpentcs.com>
+* Bhavesh Odedra <bodedra@opensourceintegrators.com>
+* Brian McMaster <brian@mcmpest.com>
--- l10n_us_form_1099/tests/test_l10n_us_form_1099.py
+++ l10n_us_form_1099/tests/test_l10n_us_form_1099.py
@@ -1,4 +1,5 @@
-# Copyright 2018 Open Source Integrators <https://opensourceintegrators.com>
+# Copyright (C) 2019 Brian McMaster
+# Copyright (C) 2019 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo.tests.common import TransactionCase
