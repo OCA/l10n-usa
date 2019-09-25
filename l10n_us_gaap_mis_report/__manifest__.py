06d9cb626e658688cbb1d7b7410ae3078c75a92a
mreficent <miquel.raich@eficent.com>
2019-09-25 17:25:40 +0200

    [MIG] l10n_us_gaap_mis_report: Migration to 12.0

--- l10n_us_gaap_mis_report/__manifest__.py
+++ l10n_us_gaap_mis_report/__manifest__.py
@@ -1,18 +1,18 @@
 # Copyright 2018 Eficent Business and IT Consulting Services, S.L.
-#                <contact@eficent.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl
 
 {
     'name': 'MIS Builder Templates for US Chart of Accounts',
     'author': 'Eficent,'
               'Odoo Community Association (OCA)',
+    'maintainers': ['jbeficent'],
     'website': 'https://github.com/OCA/l10n-usa',
     'category': 'Reporting',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'depends': [
-        'mis_builder',  # OCA/mis-builder
-        'l10n_us_gaap',  # OCA/mis-builder
+        'mis_builder',  # In OCA/mis-builder
+        'l10n_us_gaap',
     ],
     'data': [
         'data/mis_report_styles.xml',
@@ -20,4 +20,5 @@
         'data/mis_report_income_statement.xml',
     ],
     'installable': True,
+    'development_status': 'Production/Stable',
 }
