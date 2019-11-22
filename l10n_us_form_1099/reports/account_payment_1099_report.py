ac14d58031cdf368c27465ea2b3dfa0350120316
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2019-11-22 10:49:48 +0530

    [13.0][MIG] l10n_us_form_1099

--- l10n_us_form_1099/reports/account_payment_1099_report.py
+++ l10n_us_form_1099/reports/account_payment_1099_report.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2019 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, tools
+from odoo import fields, models, tools
 
 
 class AccountPayment1099Report(models.Model):
@@ -10,12 +10,11 @@ class AccountPayment1099Report(models.Model):
     _description = "1099 Payment Statistics"
     _auto = False
 
-    date = fields.Date('Payment Date', readonly=True)
-    amount = fields.Float('Payment Amount', readonly=True)
-    vendor_id = fields.Many2one('res.partner', 'Vendor', readonly=True)
-    type_1099 = fields.Many2one('type.1099', '1099 Type', readonly=True)
-    box_1099_misc = fields.Many2one('box.1099.misc', '1099-MISC Box',
-                                    readonly=True)
+    date = fields.Date("Payment Date", readonly=True)
+    amount = fields.Float("Payment Amount", readonly=True)
+    vendor_id = fields.Many2one("res.partner", "Vendor", readonly=True)
+    type_1099 = fields.Many2one("type.1099", "1099 Type", readonly=True)
+    box_1099_misc = fields.Many2one("box.1099.misc", "1099-MISC Box", readonly=True)
 
     def _select(self):
         return """
@@ -44,16 +43,16 @@ class AccountPayment1099Report(models.Model):
                 v.is_1099 = TRUE
         """
 
-    @api.model_cr
     def init(self):
         tools.drop_view_if_exists(self._cr, self._table)
-        self._cr.execute("""
+        self._cr.execute(
+            """
             CREATE OR REPLACE VIEW %s AS (
                 %s
                 %s
                 %s
                 %s
             )
-        """ % (self._table, self._select(), self._from(),
-               self._join(), self._where())
+        """
+            % (self._table, self._select(), self._from(), self._join(), self._where())
         )
