4995e2fae093d8525d9e6edc0054f042379bf98b
hveficent <hector.villarreal@forgeflow.com>
2019-12-31 12:38:15 +0100

    [IMP] l10n_us_form_1099: black, isort

--- l10n_us_form_1099/models/res_partner.py
+++ l10n_us_form_1099/models/res_partner.py
@@ -8,17 +8,16 @@ from odoo import api, fields, models
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    is_1099 = fields.Boolean('Is a 1099?')
-    type_1099_id = fields.Many2one('type.1099', string='1099 Type')
-    box_1099_misc_id = fields.Many2one('box.1099.misc',
-                                       string='1099-MISC Box')
+    is_1099 = fields.Boolean("Is a 1099?")
+    type_1099_id = fields.Many2one("type.1099", string="1099 Type")
+    box_1099_misc_id = fields.Many2one("box.1099.misc", string="1099-MISC Box")
 
-    @api.onchange('is_1099')
+    @api.onchange("is_1099")
     def _on_change_is_1099(self):
         if self.is_1099 and not self.supplier:
             self.supplier = True
 
-    @api.onchange('supplier')
+    @api.onchange("supplier")
     def _on_change_supplier(self):
         if self.is_1099 and not self.supplier:
             self.is_1099 = False
--- l10n_us_form_1099/tests/test_l10n_us_form_1099.py
+++ l10n_us_form_1099/tests/test_l10n_us_form_1099.py
@@ -6,12 +6,11 @@ from odoo.tests.common import TransactionCase
 
 
 class TestL10nUsForm1099(TransactionCase):
-
     def test_on_change_is_1099(self):
         """
             Test that supplier is True if is_1099 is True
         """
-        partner = self.env.ref('base.res_partner_2')
+        partner = self.env.ref("base.res_partner_2")
         partner.is_1099 = True
         partner._on_change_is_1099()
         self.assertTrue(partner.supplier)
@@ -20,7 +19,7 @@ class TestL10nUsForm1099(TransactionCase):
         """
             Test that is_1099 is False if supplier is False
         """
-        partner = self.env.ref('base.res_partner_2')
+        partner = self.env.ref("base.res_partner_2")
         partner.supplier = False
         partner._on_change_supplier()
         self.assertFalse(partner.is_1099)
