c76a2775ef78882d5ff0c97f0289986ac80594e4
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-02-18 12:26:12 +0100

    [MIG] l10n_us_gaap_mis_report: Migration to 13.0

--- l10n_us_gaap_mis_report/__manifest__.py
+++ l10n_us_gaap_mis_report/__manifest__.py
@@ -1,13 +1,13 @@
-# Copyright 2018 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2018-20 ForgeFlow S.L. (https://www.forgeflow.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl
 
 {
     "name": "MIS Builder Templates for US Chart of Accounts",
-    "author": "Eficent," "Odoo Community Association (OCA)",
-    "maintainers": ["jbeficent"],
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "maintainers": ["JordiBForgeFlow"],
     "website": "https://github.com/OCA/l10n-usa",
     "category": "Reporting",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["mis_builder", "l10n_us_gaap"],  # In OCA/mis-builder
     "data": [
