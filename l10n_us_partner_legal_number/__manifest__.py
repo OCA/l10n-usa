d8bde727c615d4cc1e4544fca058ce10a3af8fe0
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2021-03-02 08:43:47 -0600

    [REN] partner_legal_number -> l10n_us_partner_legal_number

---
+++ l10n_us_partner_legal_number/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2021 Open Source Integrators <csoni@opensourceintegrators.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Add Legal Number for North American Banking & Financials",
+    "summary": "Add Legal Number for North American Banking & Financials",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-usa",
+    "category": "Banking addons",
+    "depends": ["base"],
+    "data": ["views/res_partner.xml"],
+    "installable": True,
+}
