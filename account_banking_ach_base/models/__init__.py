046ab37479b8be7fd3ab0f65889c256fb9e1ef92
Chanakya Soni <chanakya.soni@serpentcs.com>
2021-03-02 16:48:08 +0530

    [IMP] Removed Legal Number field from Partner

--- account_banking_ach_base/models/__init__.py
+++ account_banking_ach_base/models/__init__.py
@@ -3,7 +3,5 @@
 from . import account_banking_mandate
 from . import account_move
 from . import account_payment_order
-from . import base
 from . import res_bank
 from . import res_company
-from . import res_partner
--- account_banking_ach_base/models/res_partner.py
+++
@@ -1,6 +0,0 @@
-from odoo import models
-
-
-class ResPartner(models.Model):
-    _name = "res.partner"
-    _inherit = ["countinghouse.legal_id_number", "res.partner"]
