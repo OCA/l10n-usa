633c895f6cc718852d37d64fd7ae9263c8dae02e
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2021-03-05 17:36:01 -0600

    [FIX] account_banking_ach_base

--- account_banking_ach_base/models/res_bank.py
+++ account_banking_ach_base/models/res_bank.py
@@ -19,13 +19,13 @@ class ResBank(models.Model):
                 rtn.validate(self.routing_number)
             except Exception:
                 raise ValidationError(
-                    _("%s is not a valid US routing " "number!" % self.routing_number)
+                    _("%s is not a valid US routing number!" % self.routing_number)
                 )
         elif country_code == "CA":
             if len(self.routing_number) != 8 or not self.routing_number.is_digit():
                 raise ValidationError(
                     _(
-                        "%s is not a valid Canadian routing "
-                        "number!" % self.routing_number
+                        "%s is not a valid Canadian routing number!"
+                        % self.routing_number
                     )
                 )
