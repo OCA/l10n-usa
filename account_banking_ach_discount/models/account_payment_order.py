958bc03d5d3d0714959a847e4e453436004976c1
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2021-03-12 16:18:48 -0600

    [FIX] account_banking_ach_discount

--- account_banking_ach_discount/models/account_payment_order.py
+++ account_banking_ach_discount/models/account_payment_order.py
@@ -16,22 +16,22 @@ class AccountPaymentOrder(models.Model):
             if "bank_payment_line_id" in vals[2] and vals[2]["bank_payment_line_id"]:
                 bank_payment_id = vals[2].get("bank_payment_line_id")
                 bank_payment = bank_payment_line_pool.browse(bank_payment_id)
-                for transaction in bank_payment.payment_line_ids:
+                for line in bank_payment.payment_line_ids:
 
                     temp_vals = vals[2].copy()
-                    amount = transaction.amount_currency
-                    discount = transaction.discount_amount
-                    payment_difference = transaction.payment_difference
+                    amount = line.amount_currency
+                    discount = line.discount_amount
+                    payment_difference = line.payment_difference
                     writeoff = (
                         payment_difference and payment_difference - discount or 0.0
                     )
-                    invoice_close = transaction.payment_difference_handling != "open"
-                    use_debit = transaction.move_id.move_type in (
+                    invoice_close = line.payment_difference_handling != "open"
+                    use_debit = line.move_id.move_type in (
                         "in_invoice",
                         "out_refund",
                     )
 
-                    temp_vals["move_id"] = transaction.move_id.id
+                    temp_vals["move_id"] = line.move_id.id
                     if use_debit:
                         temp_vals["debit"] = amount + discount
                     else:
@@ -40,8 +40,8 @@ class AccountPaymentOrder(models.Model):
                     line_ids.append((0, 0, temp_vals))
 
                     if discount > 0:
-                        discount_information = transaction.move_id.invoice_payment_term_id._check_payment_term_discount(
-                            transaction.move_id, transaction.date
+                        discount_information = line.move_id.invoice_payment_term_id._check_payment_term_discount(
+                            line.move_id, line.date
                         )
                         discount_vals = temp_vals.copy()
                         discount_vals["account_id"] = discount_information[1]
@@ -61,8 +61,8 @@ class AccountPaymentOrder(models.Model):
                             temp_vals["debit"] = amount + discount + round(writeoff, 2)
                         else:
                             temp_vals["credit"] = amount + discount + round(writeoff, 2)
-                        writeoff_vals = transaction.move_id._prepare_writeoff_move_line(
-                            transaction, temp_vals.copy()
+                        writeoff_vals = line.move_id._prepare_writeoff_move_line(
+                            line, temp_vals.copy()
                         )
                         writeoff_vals["bank_payment_line_id"] = False
                         if writeoff_vals:
@@ -70,6 +70,5 @@ class AccountPaymentOrder(models.Model):
             # payment order line
             else:
                 line_ids.append(vals)
-
         values["line_ids"] = line_ids
         return values
---
+++ account_banking_ach_discount/readme/HISTORY.rst
@@ -0,0 +1,4 @@
+12.0.1.0.0
+~~~~~~~~~~
+
+- ACH Payment, Automatic Discount and Batch Payment Partial Pay Integration
