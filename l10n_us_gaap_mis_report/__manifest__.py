fe5fb6d1f29005a0768de15f0884dafcf9db1810
Jordi Ballester <jordi.ballester@forgeflow.com>
2021-10-14 08:09:29 +0200

    [l10n_us_gaap_mis_report][mig] migration to 14.0

--- l10n_us_gaap_mis_report/__manifest__.py
+++ l10n_us_gaap_mis_report/__manifest__.py
@@ -7,7 +7,7 @@
     "maintainers": ["JordiBForgeFlow"],
     "website": "https://github.com/OCA/l10n-usa",
     "category": "Reporting",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["mis_builder", "l10n_us_gaap"],  # In OCA/mis-builder
     "data": [
