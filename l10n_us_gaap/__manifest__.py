d3c0cb63ca81efc9aff8af9ee95d95ef320e93ee
Jordi Ballester <jordi.ballester@forgeflow.com>
2021-10-14 07:43:28 +0200

    [l10n_us_gaap][mig] migration to 14.0

--- l10n_us_gaap/__manifest__.py
+++ l10n_us_gaap/__manifest__.py
@@ -1,11 +1,11 @@
 {
     "name": "United States Sample GAAP Chart of Accounts",
-    "version": "13.0.1.1.0",
+    "version": "14.0.1.1.0",
     "development_status": "Mature",
     "category": "Localization",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "maintainers": ["JordiBForgeFlow"],
-    "website": "https://github.com/OCA/l10n-usa/",
+    "website": "https://github.com/OCA/l10n-usa",
     "depends": ["account"],
     "data": [
         "data/account_chart_template_data.xml",
