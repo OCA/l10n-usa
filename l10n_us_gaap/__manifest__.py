233d8bc544d0ee67418c318e6d482b844841117e
lk-eska <leventk@eskayazilim.com.tr>
2022-10-17 23:19:19 +0300

    l10n_us_gaap: Migration to 16.0

--- l10n_us_gaap/__manifest__.py
+++ l10n_us_gaap/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "United States Sample GAAP Chart of Accounts",
-    "version": "14.0.1.1.0",
+    "version": "16.0.1.0.0",
     "development_status": "Mature",
     "category": "Localization",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
