d84fb69c803ad614a91bf12954c4c463c829722b
Murtaza Mithaiwala <mmithaiwala@opensourceintegrators.com>
2022-12-19 13:46:02 +0530

    [16.0][MIG] account_banking_ach_direct_debit
    [FIX] account_banking_ach_direct_debit: use the payment_ids field instead of bank_line_ids
    
    Apply same fix as #101

--- account_banking_ach_direct_debit/models/account_payment_order.py
+++ account_banking_ach_direct_debit/models/account_payment_order.py
@@ -30,18 +30,18 @@ class AccountPaymentOrder(models.Model):
         mandate = self.env["account.banking.mandate"]
         for order in self:
             to_expire_mandates = first_mandates = all_mandates = mandate
-            for bank_line in order.bank_line_ids:
-                if bank_line.mandate_id in all_mandates:
+            for payment in order.payment_ids:
+                if payment.mandate_id in all_mandates:
                     continue
-                all_mandates += bank_line.mandate_id
-                if bank_line.mandate_id.type == "oneoff":
-                    to_expire_mandates += bank_line.mandate_id
-                elif bank_line.mandate_id.type == "recurrent":
-                    seq_type = bank_line.mandate_id.recurrent_sequence_type
+                all_mandates += payment.mandate_id
+                if payment.mandate_id.type == "oneoff":
+                    to_expire_mandates += payment.mandate_id
+                elif payment.mandate_id.type == "recurrent":
+                    seq_type = payment.mandate_id.recurrent_sequence_type
                     if seq_type == "final":
-                        to_expire_mandates += bank_line.mandate_id
+                        to_expire_mandates += payment.mandate_id
                     elif seq_type == "first":
-                        first_mandates += bank_line.mandate_id
+                        first_mandates += payment.mandate_id
             all_mandates.write({"last_debit_date": order.date_generated})
             to_expire_mandates.write({"state": "expired"})
             first_mandates.write({"recurrent_sequence_type": "recurring"})
