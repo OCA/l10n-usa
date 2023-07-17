da0f8cd14dde5af8a473f6dbc1479668143ccf24
Dave Burkholder <dave@compassfoundation.io>
2023-07-17 11:04:07 -0400

    [FIX] remove Environment.manage

--- account_banking_ach_credit_transfer/post_install.py
+++ account_banking_ach_credit_transfer/post_install.py
@@ -5,13 +5,11 @@ from odoo import SUPERUSER_ID, api
 
 
 def update_bank_journals(cr, registry):
-    with api.Environment.manage():
-        env = api.Environment(cr, SUPERUSER_ID, {})
-        journals = env["account.journal"].search([("type", "=", "bank")])
-        ach_ct = env.ref(
-            "account_banking_ach_credit_transfer.ach_credit_transfer",
-            raise_if_not_found=False,
-        )
-        if ach_ct:
-            journals.write({"outbound_payment_method_line_ids": [(4, ach_ct.id)]})
-    return
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    journals = env["account.journal"].search([("type", "=", "bank")])
+    ach_ct = env.ref(
+        "account_banking_ach_credit_transfer.ach_credit_transfer",
+        raise_if_not_found=False,
+    )
+    if ach_ct:
+        journals.write({"outbound_payment_method_line_ids": [(4, ach_ct.id)]})
