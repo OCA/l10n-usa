76a8fa5aa7fd8ed3ffc22fab37712addd91bd3f1
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2023-10-03 16:55:38 +0530

    [MIG] l10n_us_account_routing: Migration to 16.0

--- l10n_us_account_routing/__manifest__.py
+++ l10n_us_account_routing/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Bank Routing Numbers",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Banking addons",
     "summary": "Add the routing numbers to the banks",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
