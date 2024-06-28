addbce26229ed6796d236a35b9d092ab207a5b00
Jordi Ballester Alomar <jordi.ballester@forgeflow.com>
2024-06-28 18:59:54 +0200

    [FIX] account_banking_ach_discount: Only generate write-off if there's an amout to write-off and a write-off account

--- account_banking_ach_discount/models/account_payment_line.py
+++ account_banking_ach_discount/models/account_payment_line.py
@@ -1,6 +1,8 @@
 # Copyright (C) 2019 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import models
+from odoo import _, models
+from odoo.exceptions import UserError
+from odoo.tools.float_utils import float_is_zero
 
 
 class AccountPaymentOrder(models.Model):
@@ -16,11 +18,22 @@ class AccountPaymentOrder(models.Model):
         payment_difference = self.payment_difference
         if self.payment_type == "outbound":
             payment_difference *= -1
-        write_off_line_vals = {
-            "account_id": self.writeoff_account_id.id,
-            "name": note,
-            "amount_currency": payment_difference,
-            "balance": payment_difference,
-        }
-        values["write_off_line_vals"] = [write_off_line_vals]
+        if not float_is_zero(
+            payment_difference, precision_digits=self.currency_id.decimal_places
+        ):
+            if not self.writeoff_account_id:
+                raise UserError(
+                    _(
+                        "A payment difference was found, "
+                        "but you need to indicicate a corresponding "
+                        "writeoff account."
+                    )
+                )
+            write_off_line_vals = {
+                "account_id": self.writeoff_account_id.id,
+                "name": note,
+                "amount_currency": payment_difference,
+                "balance": payment_difference,
+            }
+            values["write_off_line_vals"] = [write_off_line_vals]
         return values
