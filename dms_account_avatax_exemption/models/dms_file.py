3c8f49a34dbd1ea8d486e21668da2fffd299398f
kobros-tech <alkobroslymohamed@gmail.com>
2025-08-29 22:32:34 +0300

    [16.0][ADD] dms_account_avatax_exemption

---
+++ dms_account_avatax_exemption/models/dms_file.py
@@ -0,0 +1,20 @@
+from odoo import api, models
+
+
+class File(models.Model):
+    _inherit = "dms.file"
+
+    @api.model
+    def create(self, vals):
+        document = super().create(vals)
+        # Find a matching workflow rule
+        rule = self.env["dms.workflow.rule"].search(
+            [("dms_exemption_folder", "=", document.directory_id.parent_id.id)], limit=1
+        )
+        if (
+            rule
+            and rule.dms_exemption_folder == document.root_directory_id
+            and self.env.company.documents_exemption_settings
+        ):
+            rule.apply_rule(document)
+        return document
---
+++ dms_account_avatax_exemption/readme/USAGE.rst
@@ -0,0 +1,12 @@
+Steps
+=====
+
+#. Install the `account_avatax_exemption` module.
+#. Go to **Settings → Documents**:
+   - Check **Exemption**.
+   - Set **Workspace** as *Partner Exemptions*.
+#. Go to **Documents → Partner Exemptions**:
+   - Create or select a partner directory.
+   - Upload a file.
+
+Then the exemption is created for that partner.
