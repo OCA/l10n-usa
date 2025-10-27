38e8b2d102986c92d92c610b2b83665cb2bbbeda
Adasat Torres de León <a.torres@binhex.cloud>
2025-10-27 10:55:53 +0000

    [MIG] l10n_us_mis_financial_report: Migration to 18.0

--- l10n_us_mis_financial_report/__manifest__.py
+++ l10n_us_mis_financial_report/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Profit & Loss (US) / Balance sheet (US) MIS templates",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Binhex,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-usa",
