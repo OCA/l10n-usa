92eabdd692d8443738d45d3a005d34ddfc9c932f
Don Kendall <dkendall@ledoweb.com>
2026-05-24 10:44:57 -0400

    [IMP] l10n_us_account_routing: pre-commit auto fixes

--- l10n_us_account_routing/__manifest__.py
+++ l10n_us_account_routing/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Bank Routing Numbers",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Banking addons",
     "summary": "Add the routing numbers to the banks",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
