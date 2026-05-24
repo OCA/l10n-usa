7e5b2672ff894a7b8f3c3bbb69630a927f9175aa
Don Kendall <dkendall@ledoweb.com>
2026-05-24 10:46:44 -0400

    [IMP] account_banking_ach_base: pre-commit auto fixes

--- account_banking_ach_base/__manifest__.py
+++ account_banking_ach_base/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Localizations for North American Banking & Financials",
     "summary": "Add fields required for North American Banking & Financials",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "license": "AGPL-3",
     "author": "Thinkwell Designs, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-usa",
