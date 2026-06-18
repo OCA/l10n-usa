e3b509738f5b71ac5247812962114dc10aef555b
Adam Heinz <adam.heinz@metricwise.com>
2026-06-18 13:13:44 -0400

    [ADD] l10n_us_attomdata: Paid API access for property data.
    https://api.developer.attomdata.com/docs

---
+++ l10n_us_attomdata/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "AttomData.com API",
+    "version": "18.0.1.0.0",
+    "summary": "Paid attomdata.com API access for US property data",
+    "category": "Real Estate",
+    "application": True,
+    "author": "Odoo Community Association (OCA), MetricWise",
+    "license": "AGPL-3",
+    "maintainer": "Adam Heinz <adam.heinz@metricwise.com>",
+    "website": "https://github.com/OCA/l10n-usa",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "data/attom_data.xml",
+        "security/attom_security.xml",
+        "security/ir.model.access.csv",
+    ],
+}
